0b0c608f4d1446b95b16053e5c588908fb5f629c
dimosthenes <ece01611@g.uowm.gr>
2024-10-05 04:23:52 +0300

    initiate SpringBoot / MySQL setup

---
+++ src/main/java/com/eu/app/rest/ApiApplication.java
@@ -0,0 +1,13 @@
+package com.eu.app.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+
+}
