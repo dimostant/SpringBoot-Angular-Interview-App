82039dcb75b6dac8a3f9adad001fb26e9a6701fc
dimostant <dimos.2000@hotmail.com>
2024-10-06 23:18:29 +0300

    test repo integrity

---
+++ src/main/java/com/eu/app/rest/entities/Customer.java
@@ -0,0 +1,14 @@
+package com.eu.app.rest.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+//import lombok.Data;
+
+//@Data
+@Entity
+@Table(name = "customer")
+
+public class Customer {
+    private int test;
+}
+
