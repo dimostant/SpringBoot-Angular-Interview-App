88e0f36f2b0998aeef68ae8f4c6a487268b1b755
dimostant <dimos.2000@hotmail.com>
2024-10-10 05:13:39 +0300

    create rest of the CRUD methods, need test

--- frontend/src/app/user.service.ts
+++ frontend/src/app/user.service.ts
@@ -10,12 +10,28 @@ export class UserService {
 
   constructor() { }
 
-  private apiUrl = 'http://localhost:8080/api/users';
+  private apiUrl = 'http://localhost:8080/api/user';
 
   http = inject(HttpClient);
 
   getUsers() {
-    return this.http.get<User[]>(this.apiUrl)
+    return this.http.get<User[]>(`${this.apiUrl}s`);
+  }
+
+  getUser(id: number) {
+    return this.http.get<User>(`${this.apiUrl}/${id}`);
+  }
+
+  createUser(user: User) {
+    return this.http.post<User>(this.apiUrl, user);
+  }
+
+  updateUser(user: User) {
+    return this.http.put<User>(`${this.apiUrl}/${user.id}`, user);
+  }
+
+  deleteUser(id: number) {
+    return this.http.delete<User>(`${this.apiUrl}/${id}`);
   }
 
 }
