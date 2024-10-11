fd6ee83ed1f7334c1b97e7055b9f031147cdce4c
dimostant <dimos.2000@hotmail.com>
2024-10-11 04:16:57 +0300

    create user form, users display components, break initial html into parts

---
+++ frontend/src/app/users-display/users-display.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users-display',
+  standalone: true,
+  imports: [],
+  templateUrl: './users-display.component.html',
+  styleUrl: './users-display.component.scss'
+})
+export class UsersDisplayComponent {
+
+}
