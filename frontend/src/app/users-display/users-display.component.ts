1f1b801cb8e55bbe2f644738faf3bb5c956fc12d
dimostant <dimos.2000@hotmail.com>
2024-10-11 07:26:48 +0300

    change get users display to show api data

--- frontend/src/app/users-display/users-display.component.ts
+++ frontend/src/app/users-display/users-display.component.ts
@@ -2,6 +2,7 @@ import { Component, inject  } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
 import { UserService } from '../user.service';
+import { User } from '../user';
 
 @Component({
   selector: 'app-users-display',
@@ -14,13 +15,27 @@ import { UserService } from '../user.service';
 })
 export class UsersDisplayComponent {
   postService = inject(UserService);
-  $users = this.postService.getUsers();
+  
+  users: User[] = [];
+
+  constructor() {
+    this.postService.getUsers().subscribe({
+      next: (data) => {
+        this.users = data;
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    });
+  }
 
   onEdit() {
 
   }
 
-  onDelete() { //id: number) {
-
+  onDelete(id: number | null) {
+    if (id != null) {
+      this.postService.deleteUser(id);
+    }
   }
 }
