676fb559e28e6bde6aae5c4b1eafad2015c637cf
dimostant <dimos.2000@hotmail.com>
2024-10-11 05:55:20 +0300

    configure routing add navbar

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { UserFormComponent } from './user-form/user-form.component';
 import { UsersDisplayComponent } from './users-display/users-display.component';
+import { app } from '../../server';
+import { AppComponent } from './app.component';
 
 
 export const routes: Routes = [
     { path: 'UserForm', component: UserFormComponent },
     { path: 'UsersDisplay', component: UsersDisplayComponent },
+    //{ path: '', component: AppComponent }
 ];
