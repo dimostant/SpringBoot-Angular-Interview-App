5fc616a5be8c85b7b5c24fe47d7cc3ce2240669b
dimostant <dimos.2000@hotmail.com>
2024-10-12 14:30:01 +0300

    block old functionallity in app module

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -18,85 +18,85 @@ import { User } from './user';
 export class AppComponent {
   title = 'EDfrontend';
 
-  userObj: User = new User( null, '', '', '', new Date(), '', '');
-  userList: User[] = [];
-  userForm: FormGroup = new FormGroup({});  
+  // userObj: User = new User( null, '', '', '', new Date(), '', '');
+  // userList: User[] = [];
+  // userForm: FormGroup = new FormGroup({});  
 
   constructor() {
-    this.createForm();
-    if (this.isLocalStorageAvailable()) {
-      const oldData = localStorage.getItem('userData');
-      if (oldData != null) {
-       const parseData = JSON.parse(oldData);
-       this.userList = parseData;
-      }
-    }
+    // this.createForm();
+    // if (this.isLocalStorageAvailable()) {
+    //   const oldData = localStorage.getItem('userData');
+    //   if (oldData != null) {
+    //    const parseData = JSON.parse(oldData);
+    //    this.userList = parseData;
+    //   }
+    // }
   }
 
-  createForm() {
-    this.userForm = new FormGroup({
-      id:           new FormControl(this.userObj.id),
-      name:         new FormControl(this.userObj.name, [Validators.required]),
-      surname:      new FormControl(this.userObj.surname,[Validators.required]),
-      gender:       new FormControl(this.userObj.gender),
-      birthDate:    new FormControl(this.userObj.birthDate,[Validators.required]),
-      workAddress:  new FormControl(this.userObj.workAddress,[Validators.required]),
-      homeAddress:  new FormControl(this.userObj.homeAddress)
-    });
-  }
+  // createForm() {
+  //   this.userForm = new FormGroup({
+  //     id:           new FormControl(this.userObj.id),
+  //     name:         new FormControl(this.userObj.name, [Validators.required]),
+  //     surname:      new FormControl(this.userObj.surname,[Validators.required]),
+  //     gender:       new FormControl(this.userObj.gender),
+  //     birthDate:    new FormControl(this.userObj.birthDate,[Validators.required]),
+  //     workAddress:  new FormControl(this.userObj.workAddress,[Validators.required]),
+  //     homeAddress:  new FormControl(this.userObj.homeAddress)
+  //   });
+  // }
 
-  onSave() {
-    const oldData = localStorage.getItem('userData');
-    if (oldData != null) {
-      const parseData = JSON.parse(oldData);
-      this.userForm.controls['id'].setValue(parseData.length + 1);
-      this.userList.unshift(this.userForm.value);
-    } else {
-      this.userList.unshift(this.userForm.value);
-    }
-    localStorage.setItem('userData', JSON.stringify(this.userList));
-    this.onReset();
-  }
+  // onSave() {
+  //   const oldData = localStorage.getItem('userData');
+  //   if (oldData != null) {
+  //     const parseData = JSON.parse(oldData);
+  //     this.userForm.controls['id'].setValue(parseData.length + 1);
+  //     this.userList.unshift(this.userForm.value);
+  //   } else {
+  //     this.userList.unshift(this.userForm.value);
+  //   }
+  //   localStorage.setItem('userData', JSON.stringify(this.userList));
+  //   this.onReset();
+  // }
 
-  onEdit(user: User) {
-    this.userObj = user;
-    this.createForm();
-  }
+  // onEdit(user: User) {
+  //   this.userObj = user;
+  //   this.createForm();
+  // }
   
-  onUpdate() {
-    const record = this.userList.find((x) => x.id == this.userForm.controls['id'].value);
-    if (record != undefined) {
-      record.name = this.userForm.controls['name'].value;
-      record.surname = this.userForm.controls['surname'].value;
-      //other fields
-    }
-    localStorage.setItem('userData', JSON.stringify(this.userList));
-    this.userObj = new User( null, '', '', '', new Date(), '', '');
-    this.createForm();
-  }
+  // onUpdate() {
+  //   const record = this.userList.find((x) => x.id == this.userForm.controls['id'].value);
+  //   if (record != undefined) {
+  //     record.name = this.userForm.controls['name'].value;
+  //     record.surname = this.userForm.controls['surname'].value;
+  //     //other fields
+  //   }
+  //   localStorage.setItem('userData', JSON.stringify(this.userList));
+  //   this.userObj = new User( null, '', '', '', new Date(), '', '');
+  //   this.createForm();
+  // }
 
-  onDelete(id: number | null) {
-    const isDelete = confirm('Are you sure you want to delete?');
-    if (isDelete) {
-      const index = this.userList.findIndex((x) => x.id == id);
-      this.userList.splice(index,1)
-      localStorage.setItem('userData', JSON.stringify(this.userList));
-    }
-  }
+  // onDelete(id: number | null) {
+  //   const isDelete = confirm('Are you sure you want to delete?');
+  //   if (isDelete) {
+  //     const index = this.userList.findIndex((x) => x.id == id);
+  //     this.userList.splice(index,1)
+  //     localStorage.setItem('userData', JSON.stringify(this.userList));
+  //   }
+  // }
 
-  onReset() {
-    this.userObj = new User( null, '', '', '', new Date(), '', '');
-    this.createForm();
-  }
+  // onReset() {
+  //   this.userObj = new User( null, '', '', '', new Date(), '', '');
+  //   this.createForm();
+  // }
 
-  isLocalStorageAvailable(): boolean {
-    try {
-      const testKey = 'test';
-      localStorage.setItem(testKey, 'testValue');
-      localStorage.removeItem(testKey);
-      return true;
-    } catch (e) {
-      return false;
-    }
-  }
+  // isLocalStorageAvailable(): boolean {
+  //   try {
+  //     const testKey = 'test';
+  //     localStorage.setItem(testKey, 'testValue');
+  //     localStorage.removeItem(testKey);
+  //     return true;
+  //   } catch (e) {
+  //     return false;
+  //   }
+  // }
 }
