4a0308d938bb884802c3598858250722497d88bf
dimostant <dimos.2000@hotmail.com>
2024-10-13 22:31:20 +0300

    add home component html and link to app component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,14 +2,12 @@ import { Routes } from '@angular/router';
 import { UserFormComponent } from './user-form/user-form.component';
 import { UsersDisplayComponent } from './users-display/users-display.component';
 import { UserDisplayComponent } from './user-display/user-display.component';
-
-import { app } from '../../server';
-import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 
 export const routes: Routes = [
     { path: 'UserForm', component: UserFormComponent },
     { path: 'UsersDisplay', component: UsersDisplayComponent },
     { path: 'UserDisplay', component: UserDisplayComponent },
-    //{ path: '', component: AppComponent }
+    { path: '', component: HomeComponent }
 ];
