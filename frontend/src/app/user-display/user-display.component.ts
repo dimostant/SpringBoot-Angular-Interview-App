9659120d669467eba8e24ae3af10acec56aa76b9
dimostant <dimos.2000@hotmail.com>
2024-10-13 16:58:28 +0300

    add user component

---
+++ frontend/src/app/user-display/user-display.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-display',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-display.component.html',
+  styleUrl: './user-display.component.scss'
+})
+export class UserDisplayComponent {
+
+}
